bdab95f7ed0d579ec9bb20eb59735c54d12ab83d
aguskuster <61338886+aguskuster@users.noreply.github.com>
2021-03-07 13:13:05 -0300

    Delete src directory

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const app = require('./config/server');
-
-require('./app/routes/products')(app);
-
-
-//Start the server
-app.listen(app.get('port'), ()=>{
-    console.log('server on port: ', app.get('port'));
-});
