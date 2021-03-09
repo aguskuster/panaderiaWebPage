bc85b17fdcd38b5a8d825c090d09b002c7fa1942
aguskuster <61338886+aguskuster@users.noreply.github.com>
2021-03-09 12:15:38 -0300

    Update server.js

--- server.js
+++ server.js
@@ -15,11 +15,11 @@ app.set('views','views');
 app.set('view engine','ejs');
 
 app.get('/', (req, res) => {
-    res.render('index', {home:'active'});
+    res.render('index');
 });
 
 app.get('/productos',(req,res) => {
-    res.render('productos', {productos:'active'});
+    res.render('productos');
 });
 app.get('/productos', async (req, res) => {
     try {
