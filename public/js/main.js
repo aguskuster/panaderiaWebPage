b110be068a658edeae9410521994587815fed44a
gerburgos <68874865+gerburgos@users.noreply.github.com>
2021-03-09 10:46:07 -0300

    new update

---
+++ public/js/main.js
@@ -0,0 +1,6 @@
+const image = document.querySelectorAll('img');
+image.forEach(play => play.addEventListener('click', () => {
+
+    play.classList.toggle('active');
+    
+}));
---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const bp = require('body-parser');
+const pool = require('./database');
+
+
+app.use(express.static('public'));
+app.use('/css', express.static(__dirname+'public/css'));
+app.use('/js', express.static(__dirname+'public/js'));
+app.use('/img', express.static(__dirname+'public/img'));
+
+
+app.set('views','views');
+app.set('view engine','ejs');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/productos',(req,res) => {
+    res.render('productos');
+});
+app.get('/productos', async (req, res) => {
+    try {
+        // get Connection
+        const conn = await pool.getConnection();
+
+        // Create Query 
+        const query = 'select * from productos';
+
+        //executing query
+        const rows = await conn.query(query);
+        //result
+        res.status(200).json(rows);
+    } catch (error) {
+        console.log(error);
+    }
+});
+app.use(bp.json());;
+app.post('/new-product', async (req, res) => {
+    console.log(req.body);
+    
+    // get Connection
+    const conn = await pool.getConnection();
+
+     // Create Query 
+     const query = 'INSERT INTO productos (nombre,descripcion,precioK,precioU) VALUES (?,?,?,?)';
+
+    //executing query
+    const result = await conn.query(query, [req.body.nombre, req.body.descripcion, req.body.precioK, req.body.precioU]);
+    //result
+    res.status(200).json(result);
+});
+
+
+app.listen(3000, () => {
+    console.log('Server on port', 3000);
+});
